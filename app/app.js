2099a514ba81d3196828fb2a7937d0374f00e2b6
Javlon1221 <avazbekzalilov835@gmail.com>
2025-04-21 02:52:55 +0500

    3-topshiriqni qildim 2-siga tushunmadim

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+// 1 - topshiriq
+
 const mainImgEl = document.querySelector(".carusel__img img");
 
 function changeImg(path) {
@@ -6,4 +8,28 @@ function changeImg(path) {
     setTimeout(() => {
         mainImgEl.setAttribute("src", path);
         mainImgEl.classList.remove("fade");
-    }, 400); }
+    }, 400); 
+}
+    
+
+// 2 -topshiriq
+ 
+function addCard() {
+    const wrapper = document.querySelector(".card-wrapper");
+
+    const cardHTML = `
+        <div class="cards">
+            <div class="cards_img">
+                <img src="./img/woman.jpg" alt="">
+            </div>
+            <div class="cards_txt">
+                <h2>Joy Parker</h2>
+                <p>UI/UX | Front-End DevelopeR</p>
+                <button class="cards_btn">See more</button>
+            </div>
+        </div>
+    `;
+
+    wrapper.insertAdjacentHTML("beforeend", cardHTML);
+}
+    
